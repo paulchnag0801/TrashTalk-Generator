fa011ddbfca8713fc343672311ea84b2e4d4c6c8
paulchnag0801 <x1232178987@gmail.com>
2021-07-08 19:08:47 +0800

    feat: setting body-parser & import generateTrashTalk function & fix post route

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const generateTrashTalk = require('./models/generate_trashtalk')
 const app = express()
 const port = 4000
 
+// setting body-parser
+app.use(express.urlencoded({ extended: true }))
+
+// setting routes
 app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.post('/', (req, res) => {
+  const options = req.body
+  const trashTalk = generateTrashTalk(options)
+  res.render('index', { trashTalk: trashTalk, options: options })
+})
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
 app.listen(port, () => {
   console.log(`The Express server is running on http://localhost${port}`)
 })
